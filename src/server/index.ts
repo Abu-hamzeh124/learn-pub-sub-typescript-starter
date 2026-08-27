fe8616c50cf4679903b1ec00378f679229d61534
Abu-hamzeh124 <ingtgammer2@gmail.com>
2026-08-27 09:56:01 +0300

    new

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,38 @@
 import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
-import { ExchangePerilDirect } from "../internal/routing/routing.js";
+import {
+  ExchangePerilDirect,
+  ExchangePerilTopic,
+  GameLogSlug,
+} from "../internal/routing/routing.js";
 import { PauseKey } from "../internal/routing/routing.js";
 import type { PlayingState } from "../internal/gamelogic/gamestate.js";
 import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
+
 async function main() {
   const connStr = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(connStr);
   const ch = await conn.createConfirmChannel();
-  const playingState: PlayingState = {
-    isPaused: true,
-  };
+  const gameLogs = await declareAndBind(
+    conn,
+    ExchangePerilTopic,
+    GameLogSlug,
+    GameLogSlug + ".*",
+    SimpleQueueType.Durable,
+  );
   console.log("Connection is successful!");
-  await publishJSON(ch, ExchangePerilDirect, PauseKey, playingState);
   printServerHelp();
+  process.on("SIGINT", async () => {
+    console.log("Shutting down the connection...");
+    try {
+      await conn.close();
+    } catch (err) {
+      console.error("Error closing connection:", err);
+    } finally {
+      process.exit(0);
+    }
+  });
   while (true) {
     const input = await getInput();
     if (input.length === 0) {
@@ -21,21 +40,18 @@ async function main() {
     }
     if (input[0] === "pause") {
       console.log("Sending pause message");
-      await publishJSON(ch, ExchangePerilDirect, PauseKey, playingState);
+      await publishJSON(ch, ExchangePerilDirect, PauseKey, { isPaused: true });
     } else if (input[0] === "resume") {
       console.log("Sending resume message");
       await publishJSON(ch, ExchangePerilDirect, PauseKey, { isPaused: false });
     } else if (input[0] === "quit") {
       console.log("Quiting");
+      await conn.close();
       break;
     } else {
       console.log("unknown command");
     }
   }
-  process.on("SIGINT", () => {
-    console.log("Shutting Down the connection.");
-    conn.close();
-  });
 }
 
 main().catch((err) => {
